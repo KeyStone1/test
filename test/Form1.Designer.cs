cb6281c0b358c264594d8e8511d6a44e5a934050
MyDesign <andy90099@msn.com>
2016-03-19 23:06:15 +0800

    Combine EMGU with Imatest-Colorcheck 1th(Slow)

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace test
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imbxCapture = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbMsg = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imbxCapture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imbxCapture
+            // 
+            this.imbxCapture.Location = new System.Drawing.Point(12, 12);
+            this.imbxCapture.Name = "imbxCapture";
+            this.imbxCapture.Size = new System.Drawing.Size(720, 480);
+            this.imbxCapture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.imbxCapture.TabIndex = 2;
+            this.imbxCapture.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(752, 13);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(107, 31);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbMsg
+            // 
+            this.tbMsg.Location = new System.Drawing.Point(752, 73);
+            this.tbMsg.Multiline = true;
+            this.tbMsg.Name = "tbMsg";
+            this.tbMsg.Size = new System.Drawing.Size(121, 87);
+            this.tbMsg.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 511);
+            this.Controls.Add(this.tbMsg);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.imbxCapture);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imbxCapture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imbxCapture;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbMsg;
+    }
+}
+
